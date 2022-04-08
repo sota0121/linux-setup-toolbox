747413d1ef4b8df0e8b95c84df6a98a9c63a0469
sota_masuda <sota.m777121@gmail.com>
2022-04-08 13:00:16 +0900

    add custom-install.sh

---
+++ ubuntu/custom-install.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+# Google Chrome
+echo "Install google-chrome ..."
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+
