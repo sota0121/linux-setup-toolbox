82eef6adde610d62b6208060b1d99ed905d97f01
sota_masuda <sota.m777121@gmail.com>
2022-04-09 16:56:12 +0900

    update method of installing code (because, snap pkg has bugs, doesnt work jp input)

--- ubuntu/custom-install.sh
+++ ubuntu/custom-install.sh
@@ -26,6 +26,20 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 echo "setup zshrc ..."
 cp ./.zshrc.template ~/.zshrc
 
+
+# code
+echo "Install code ..."
+sudo apt-get install wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
+
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install code
+
+
 # Pyenv
 echo "Install pyenv dependencies ..."
 sudo apt-get update; sudo apt-get install make build-essential libssl-dev zlib1g-dev \
